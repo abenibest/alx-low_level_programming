1e878011f6ddb4ee658cf2d8d96cd1fa572b3f7c
abenibest <abenezerabrha97@gmail.com>
2023-08-15 15:01:40 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,13 +8,14 @@
  * -1 and prints - if n is less than zero
  */
 int print_sign(int n)
-{
+
+        {
 	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	} else if (n == 0)
-}
+        }
 		_putchar(48);
 		return (0);
 	{ else if (n < 0)
@@ -22,4 +23,4 @@ int print_sign(int n)
 		_putchar('-');
 	}
 		return (-1);
-}
+        }
